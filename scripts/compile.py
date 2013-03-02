fd9e84a8be254c8bf66d455f5032c619e6422f59
Paul Baumstarck <pbaumstarck@gmail.com>
2013-03-02 12:10:21 -0800

    Added fix to the compilation script to escape the 'compiled.js' file itself by scanning the HTML template better.

--- scripts/compile.py
+++ scripts/compile.py
@@ -5,14 +5,18 @@ import httplib, urllib, re, sys
 # Scrape the template page to figure out all the JS we want to include
 lines = open('../index.html', 'r').readlines()
 scripts = []
+# Only start keeping scripts after we've seen an 'else'
+seen_else = False
 for line in lines:
-  src_match = re.match(r'.*type\s*=\s*"text/javascript".*?\ssrc\s*=\s*"([^"]+)".*', line)
-  if src_match:
-   scripts.append(src_match.group(1))
+  if seen_else:
+    src_match = re.match(r'.*type\s*=\s*"text/javascript".*?\ssrc\s*=\s*"([^"]+)".*', line)
+    if src_match:
+     scripts.append(src_match.group(1))
+  elif re.match(r'.*{% else %}.*', line):
+    seen_else = True
 
 source = '\n'.join([open('..' + script, 'r').read() for script in scripts
     if re.match(r'^/javascript/.*', script)])
-
 
 # Define the parameters for the POST request and encode them in a URL-safe
 # format.
@@ -30,6 +34,9 @@ conn.request('POST', '/compile', params, headers)
 response = conn.getresponse()
 data = response.read()
 open('../javascript/compiled.js', 'w').write(data)
+print "Old data: " + str(len(source))
+print "New data: " + str(len(data))
+print "%% reduction: " + str(float(int(10000 * (len(source) - len(data)) / len(source))) / 100.0)
 #print data
 conn.close()
 
